7b2d58ced3ecdb8c3470651ebb3a261ee27e36ed
Mukoua <mukoua@gmail.com>
2025-08-30 00:17:28 -0300

    Criada uma entidade no NinjaModel

---
+++ CadastroDeNinjas/src/main/java/dev/Anderson/CadastroDeNinjas/NinjaController.java
@@ -0,0 +1,17 @@
+package dev.Anderson.CadastroDeNinjas;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class NinjaController {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota!!";
+
+    }
+}
