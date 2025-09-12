2eec4e8c439d8f2a96166a3a28c7e1a9707ba645
Mukoua <mukoua@gmail.com>
2025-09-12 20:13:12 -0300

    chore: .gitignore adiciona as minhas credenciais do db

---
+++ CadastroDeNinjas/src/main/java/dev/Anderson/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -0,0 +1,7 @@
+package dev.Anderson.CadastroDeNinjas.Ninjas;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
+}
